fbdde7350110dbdd6314b6d1f627acefba87586e
Dustin Micklos <damicklo5@gmail.com>
2021-04-21 15:06:55 -0500

    webpack enhancements and image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 //using a core Node.js module that we've used previously, called path. Webpack uses Node.js to build our application, so we can use npm modules and the require module.
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -13,6 +14,9 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),  
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
